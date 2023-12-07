fc648f5143dee71d931ba5cdf42b51124da6b39a
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-12-07 09:49:58 +0530

    Test api

---
+++ src/pages/api/energyData.js
@@ -0,0 +1,26 @@
+// pages/api/energyData.js
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { voltage, current, power, energy } = req.body;
+
+      // Process the incoming data as needed
+      console.log("Received data from ESP32:");
+      console.log(
+        `Voltage: ${voltage}V, Current: ${current}A, Power: ${power}W, Energy: ${energy}kWh`
+      );
+
+      // You can store the data in a database, perform additional calculations, etc.
+
+      // Respond with a success message
+      res.status(200).json({ message: "Data received successfully" });
+    } catch (error) {
+      console.error("Error processing data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    // Return a method not allowed error for non-POST requests
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
